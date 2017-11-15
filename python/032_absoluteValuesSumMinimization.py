27a4d9643215bfb6704c812ee41ee3d293e71a71
Ernesto Rubio Fuentes <neto.r27@gmail.com>
2017-11-15 17:33:30 -0600

    Adding 2 more solutions

---
+++ python/032_absoluteValuesSumMinimization.py
@@ -0,0 +1,11 @@
+def absoluteValuesSumMinimization(a):
+    '''
+    Given a sorted array of integers a, find an integer x from a such that the value of    
+    abs(a[0] - x) + abs(a[1] - x) + ... + abs(a[a.length - 1] - x)    
+    is the smallest possible (here abs denotes the absolute value).
+    If there are several possible answers, output the smallest one.
+    '''
+    return a[(len(a) - 1) // 2]
+
+
+print(absoluteValuesSumMinimization([2, 4, 7]))
