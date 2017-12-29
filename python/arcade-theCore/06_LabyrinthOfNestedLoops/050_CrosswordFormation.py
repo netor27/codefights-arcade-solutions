c7c686cf2667b4d54c6b10e67389bf014f823682
Ernesto Rubio Fuentes <neto.r27@gmail.com>
2017-12-28 22:57:00 -0600

    modifying solution, still not good enough

--- python/arcade-theCore/06_LabyrinthOfNestedLoops/050_CrosswordFormation.py
+++ python/arcade-theCore/06_LabyrinthOfNestedLoops/050_CrosswordFormation.py
@@ -1,59 +1,69 @@
 import itertools
-def crosswordFormation(words):
+import timeit
+
+def crosswordFormation(words):    
+    w1, w2, w3, w4 = words
+    r = 0
+    # sum the possible combinations if we pair w1 and w2 in opposite sides and w3 with w4. Then multiply it by 8 (8 possible rotations of the square)
+    #r += sumOfCombinations(w1, w3, w2, w4) * 8
+
+    # sum the possible combinations if we pair w1 and w3 in opposite sides and w2 with w4. Then multiply it by 8 (8 possible rotations of the square)
+    #r += sumOfCombinations(w1, w2, w3, w4) * 8
+
+    # sum the possible combinations if we pair w2 and w3 in opposite sides and w1 with w4. Then multiply it by 8 (8 possible rotations of the square)
+    #r += sumOfCombinations(w2, w1, w3, w4) * 8
+
     r = 0
     for w in itertools.permutations(words):
-        print(w)
-        #print("\t", w[0])
-        for s1 in range(len(w[0]) - 2):
-            ps1 = [i for i,p in enumerate(w[1]) if p == w[0][s1]]
-            #if len(ps1) > 0:
-                #print("\t\t", w[0][s1], " with ", w[1])
-                #print("\t\t", ps1)                
-            for s2 in range(s1 + 2, len(w[0])):
-                ps2 = [i for i,p in enumerate(w[2]) if p == w[0][s2]]
-                #if len(ps2) > 0:
-                    #print("\t\t\t", w[1][s2], " with ", w[2])
-                    #print("\t\t\t", ps2)
-                for pw1 in ps1:
-                    for pw2 in ps2:
-                        #print(w[0][s1], w[1][s2], pw1, pw2)
-                        for c1, c2 in zip(w[1][pw1+2:], w[2][pw2+2:]):
-                            for c3, c4 in zip(w[0], w[3][s2 - s1:]):
-                                r += c1 == c3 and c2 == c4
+        r += sumOfCombinations(*w)
     return r
 
 
-print(crosswordFormation(["crossword", 
- "square", 
- "formation", 
- "something"]))
-# import ?????????
-# def crosswordFormation(words):
-#     r = 0
-#     ??? w in itertools.permutations(words):
-#         ??? s1 in range(len(w[?]) - 2):
-#             ps1 = [i for i,? in enumerate(w[1]) if ? == w[0][s1]]
-#             for s2 ?? range(s1 + 2, len(w[?])):
-#                 ps2 = [i for i,c ?? enumerate(w[2]) if c == ?[0][s2]]
-#                 for pw1 in ???:
-#                     for pw2 in ps2:
-#                         for ??, c2 in zip(w[1][???+2:], w[2][pw2+2:]):
-#                             ??? c3, c4 in zip(w[?], w[3][s2 - s1:]):
-#                                 r += ?? == c3 and c2 == c4
-#     return ?
+def sumOfCombinations(left, up, right, bottom):
+    lset = set(left)
+    rset = set(right)
+    uset = set(up)
+    bset = set(bottom)
+    r = 0
+    for l1 in range(len(left) - 2):
+        if left[l1] in uset:            
+            for u1 in range(len(up) - 2):
+                if left[l1] == up[u1]:
+                    # we found a letter that match between left and up
+                    # now, check from u1+2 till the end to find matches with right
+                    for u2 in range(u1 + 2, len(up)):
+                        if up[u2] in rset:
+                            for r1 in range(len(right) - 2):
+                                if up[u2] == right[r1]:
+                                    # we found a letter that match between up and right
+                                    # now, check from r1+2 till the end to find matches with bottom
+                                    for r2 in range(r1 + 2, len(right)):
+                                        if right[r2] in bset:
+                                            for b1 in range(u2 - u1, len(bottom)):
+                                                if right[r2] == bottom[b1]:
+                                                    # we found a letter that matches with bottom
+                                                    # now, check if by positioning bottom like this, it matches with left
+                                                    # first check if the positioning of b at least covers the distance of upper
+                                                    width = u2 - u1
+                                                    height = r2 - r1
+                                                    if b1 - width >= 0 and l1 + height < len(left):
+                                                        if left[l1 + height] == bottom[b1 - width]:
+                                                            r += 1
+    return r
 
 
 
 
-# from functools import reduce
-# from ????????? import permutations
+def test():
+# print(crosswordFormation(["crossword", 
+#  "square", 
+#  "formation", 
+#  "something"]))
+    result = crosswordFormation(["aaaaaaaaaaaaaa", 
+         "aaaaaaaaaaaaab", 
+         "aaaaaaaaaaaaca", 
+         "aaaaaaaaaaadaa"])
+    print(result)
 
-# # horizontal rotate clockwise, ???????? rotate counterclockwise
-# # top becomes left, ???? becomes top
-# # (t,l,r,?) == (l,t,b,r)
-
-# def ??????????(top,left,right,bot):
-#     return ???([sum([sum([sum([sum([sum([? for b1 in range(len(???)+t1-t2) if bot[b1] == ????[l2] and bot[b1+t2-??] == right[r1+l2-l1]]) for ?? in range(l1+2,min(???(left),l1+len(right)-r1))]) ??? l1 in range(len(left)-?) if top[t1]==left[l1]]) ??? t1 in range(t2-1)]) ??? t2 in range(2,len(???)) if top[t2]==right[r1]]) ??? r1 in range(len(right)-?)])
-
-# def crosswordFormation(words):
-#     return 2*???(numSquares(*p) for p in ????????????(words) if p[0] < p[?])
+from __main__ import test
+print(timeit.timeit(test, number=1))
