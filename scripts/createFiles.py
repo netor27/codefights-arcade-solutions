bd8b082bc5d78b8ef8d8cf0b4c3841b2ae0d5f6c
Ernesto Rubio Fuentes <neto.r27@gmail.com>
2018-01-05 10:38:22 -0600

    Adding arcade folder

--- scripts/createFiles.py
+++ scripts/createFiles.py
@@ -4,7 +4,7 @@ def main():
     fileNames = ["088_ArrayConversion", "089_ArrayPreviousLess", "090_PairofShoes", "091_Combs", "092_StringsCrossover",
                  "093_CyclicString", "094_BeautifulText", "095_RunnersMeetings", "096_ChristmasTree", "097_FileNaming"]
 
-    directory = "./python/arcade-theCore/11_SprintOfIntegration/"
+    directory = "../arcade/python/arcade-theCore/11_SprintOfIntegration/"
 
     for name in fileNames:
         fullName = directory + name + ".py"
