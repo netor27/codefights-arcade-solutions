7702f8f7773fee5308c25dba0500f2fe0fe76bdd
Ernesto Rubio <ernesto.rubio@uber.com>
2018-12-24 14:47:35 -0800

    Finish edge of the ocean part

--- arcade/go/arcade-intro/02_Edge of the Ocean/008_matrixElementsSum.go
+++ arcade/go/arcade-intro/02_Edge of the Ocean/008_matrixElementsSum.go
@@ -1,1 +1,17 @@
-# Not implemented yet!
+func matrixElementsSum(matrix [][]int) int {
+	total := 0
+	columns := len(matrix[0])
+	rows := len(matrix)
+
+	for y := 0; y < columns; y++ {
+		for x := 0; x < rows; x++ {
+			if matrix[x][y] == 0 {
+				break
+			}
+
+			total = total + matrix[x][y]
+		}
+	}
+
+	return total
+}
