5f9844135dacd0f6733e8d65f09224a948093b07
Ernesto Rubio <ernesto.rubio@uber.com>
2018-12-24 14:34:18 -0800

    Add increasing sequence solution

--- arcade/go/arcade-intro/02_Edge of the Ocean/007_almostIncreasingSequence.go
+++ arcade/go/arcade-intro/02_Edge of the Ocean/007_almostIncreasingSequence.go
@@ -1,1 +1,31 @@
-# Not implemented yet!
+func almostIncreasingSequence(sequence []int) bool {
+	j := analizeArray(sequence)
+	if j == -1 {
+		return true
+	}
+
+	b := make([]int, len(sequence))
+	copy(b, sequence)
+	if analizeArray(append(b[:j], b[j+1:]...)) == -1 {
+		return true
+	}
+
+	b = make([]int, len(sequence))
+	copy(b, sequence)
+	if analizeArray(append(b[:j+1], b[j+2:]...)) == -1 {
+		return true
+	}
+
+	return false
+}
+
+func analizeArray(array []int) int {
+	// Returns the first index of a pair of elements where the earlier element
+	// is not less than the later elements. If no such pair exists, return -1.'''
+	for i := 0; i < len(array)-1; i++ {
+		if array[i] >= array[i+1] {
+			return i
+		}
+	}
+	return -1
+}
